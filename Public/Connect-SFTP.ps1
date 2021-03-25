feff56c10a4b1f63b80c672f03a30be87fa7ec0f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-25 10:29:48 +0100

    Update data

---
+++ Public/Connect-SFTP.ps1
@@ -0,0 +1,46 @@
+﻿function Connect-SFTP {
+    [cmdletBinding(DefaultParameterSetName = 'Password')]
+    param(
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [string] $Server,
+
+        [Parameter(ParameterSetName = 'ClearText')]
+        [string] $Username,
+
+        [Parameter(ParameterSetName = 'ClearText')]
+        [string] $Password,
+
+        [Parameter(ParameterSetName = 'Password')]
+        [pscredential] $Credential,
+
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [int] $Port
+    )
+
+    if ($Username -and $Password) {
+        $SftpClient = [Renci.SshNet.SftpClient]::new($Server, $Username, $Password)
+    } elseif ($Credential) {
+        $SftpClient = [Renci.SshNet.SftpClient]::new($Server, $Credential.Username, $Credential.GetNetworkCredential().Password)
+    } else {
+        throw 'Not implemented. Add certificate'
+    }
+    if ($Port) {
+        $SftpClient.Port = $Port
+    }
+
+    try {
+        $SftpClient.Connect()
+        $SftpClient | Add-Member -Name 'Error' -Value $null -Force -MemberType NoteProperty
+    } catch {
+        $SftpClient | Add-Member -Name 'Error' -Value $($_.Exception.Message) -Force -MemberType NoteProperty
+        if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+            Write-Error $_
+            return
+        } else {
+            Write-Warning "Connect-SFTP - Error: $($_.Exception.Message)"
+        }
+    }
+    $SftpClient
+}
---
+++ Public/Get-SFTPList.ps1
@@ -0,0 +1,25 @@
+﻿function Get-SFTPList {
+    [cmdletBinding()]
+    param(
+        [alias('FtpPath')][string] $Path,
+        [Parameter(Mandatory)][Renci.SshNet.SftpClient] $SftpClient
+    )
+    if ($SftpClient -and $SftpClient.IsConnected -and -not $SftpClient.Error) {
+        try {
+            if ($Path) {
+                $SftpClient.ListDirectory($Path)
+            } else {
+                $SftpClient.ListDirectory('')
+            }
+        } catch {
+            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+                Write-Error $_
+                return
+            } else {
+                Write-Warning "Get-SFTPList - Error: $($_.Exception.Message)"
+            }
+        }
+    } else {
+        Write-Warning "Get-SFTPList - Skipped (IsConnected $($SftpClient.IsConnected) / Error: $($SftpClient.Error))"
+    }
+}
