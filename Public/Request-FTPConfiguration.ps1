fa385ffda7fb3d76a78e5a39633595daec6e31f5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-26 20:38:40 +0100

    Improvement docs

--- Public/Request-FTPConfiguration.ps1
+++ Public/Request-FTPConfiguration.ps1
@@ -1,4 +1,63 @@
 ﻿function Request-FTPConfiguration {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Automatically discover working FTP connection settings and return those connection profiles. This method will try every possible connection type combination in a loop until it finds a working combination, and it will return the first found combination or all found combinations. The connection types are tried in this order of preference.
+
+    Auto connection attempts to find working connection settings in this order of preference:
+
+    Protocol Preference:
+    1. None - Let the OS decide which TLS/SSL version to use
+    2. Tls12 - TLS 1.2 (TLS 1.3 is not yet stable in .NET Framework)
+    3. Tls11 - TLS 1.1
+    4. Tls - TLS 1.0
+    5. Ssl3 - SSL 3.0 (obsolete, need to use TLS instead)
+    6. Ssl2 - SSL 2.0 (obsolete, need to use TLS instead)
+    7. Default - Undefined/weird behaviour
+
+    Data Connection Type Preference:
+
+    1. PASV - We prefer passive as its the most reliable
+    2. EPSV - Enhanced passive is not as well supported on servers
+    3. PORT - PORT is an older connection type
+    4. EPRT - Enhanced PORT is not as well supported on servers
+    5. PASVEX
+
+    Encoding Type Preference:
+
+    1. UTF8 - We prefer Unicode encoding as there will be no issues with file and folder names
+    2. ASCII - ASCII/ANSI is a fallback used for older servers
+
+    .PARAMETER Server
+    Server Name or IP Address to Connect
+
+    .PARAMETER Username
+    UserName for FTP Connection
+
+    .PARAMETER Password
+    Password for FTP Connection (cleartext)
+
+    .PARAMETER Credential
+    UserName and Password in form of Credentials
+
+    .PARAMETER FirstOnly
+    Returns first working profile
+
+    .EXAMPLE
+    # Login via UserName/Password
+    $ProfileFtp1 = Request-FTPConfiguration -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password'
+    $ProfileFtp1 | Format-Table
+
+    .EXAMPLE
+    # Anonymous login
+    $ProfileFtp2 = Request-FTPConfiguration -Server 'speedtest.tele2.net' -Verbose
+    $ProfileFtp2 | Format-Table
+
+    .NOTES
+    General notes
+    #>
     [cmdletBinding(DefaultParameterSetName = 'Password')]
     param(
         [Parameter(ParameterSetName = 'ClearText')]
