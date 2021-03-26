11b4aa0b203ec4f07314889582925b7b228d1987
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-26 20:23:15 +0100

    Example updates

---
+++ Examples/Example03-AutoDetectFTPConfiguration.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\Transferetto.psd1 -Force
+
+# Login via UserName/Password
+$ProfileFtp1 = Request-FTPConfiguration -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password'
+$ProfileFtp1 | Format-Table
+
+# Anonymous login
+$ProfileFtp2 = Request-FTPConfiguration -Server 'speedtest.tele2.net' -Verbose
+$ProfileFtp2 | Format-Table
---
+++ Examples/Example04-ConnectWithAutoDetect.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module .\Transferetto.psd1 -Force
+
+# Login via UserName/Password via autodetect - keep in mind using Connect-FTP directly will be faster...
+$ProfileFtp1 = Request-FTPConfiguration -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password'
+$ProfileFtp1 | Format-Table
+
+# use first profile
+$Client = Connect-FTP -FtpProfile $ProfileFtp1[0]
+Get-FTPList -Client $Client | Format-Table
+Disconnect-FTP -Client $Client
