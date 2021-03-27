3558335cfca23606e9d5aca3ffcdcb3353c99fcc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-27 19:59:57 +0100

    Update examples

---
+++ Examples/Example06-UploadFTPS.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module .\Transferetto.psd1 -Force
+
+$Client = Connect-FTP -Server '192.168.241.187' -Verbose -Username 'test' -Password 'BiPassword90A' -EncryptionMode Explicit -ValidateAnyCertificate
+$List = Get-FTPList -Client $Client
+$List | Format-Table
+$List = Get-FTPList -Client $Client -Path '/Temporary'
+$List | Format-Table
+
+$ListFiles = Get-ChildItem -LiteralPath $PSScriptRoot\Upload
+foreach ($File in $ListFiles) {
+    Add-FTPFile -Client $Client -LocalPath $File.FullName -RemotePath "/Temporary/$($File.Name)" -RemoteExists Skip
+    Add-FTPFile -Client $Client -LocalPath $File.FullName -RemotePath "/Temporary/CreateDir/$($File.Name)" -RemoteExists Skip -CreateRemoteDirectory
+}
+
+Disconnect-FTP -Client $Client
---
+++ Examples/Example07-UploadSFTP.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module .\Transferetto.psd1 -Force
+
+$SftpClient = Connect-SFTP -Server '192.168.241.187' -Verbose -Username 'test' -Password 'BiPassword90A'
+Get-SFTPList -SftpClient $SftpClient | Format-Table
+Get-SFTPList -SftpClient $SftpClient -Path "/Temporary" | Format-Table *
+
+$ListFiles = Get-ChildItem -LiteralPath $PSScriptRoot\Upload
+foreach ($File in $ListFiles) {
+    Add-SFTPFile -SftpClient $SftpClient -LocalPath $File.FullName -RemotePath "/Temporary/$($File.Name)" -AllowOverride
+}
+
+Disconnect-SFTP -SftpClient $SftpClient
