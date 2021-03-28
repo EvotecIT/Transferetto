7cfb4de9329915c38518d55ab808cff04378a93c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-28 22:32:32 +0200

    Added example

--- Examples/Example06-UploadFTPS.ps1
+++ Examples/Example06-UploadFTPS.ps1
@@ -1,15 +1,25 @@
 ﻿Import-Module .\Transferetto.psd1 -Force
 
 $Client = Connect-FTP -Server '192.168.241.187' -Verbose -Username 'test' -Password 'BiPassword90A' -EncryptionMode Explicit -ValidateAnyCertificate
+# List files
 $List = Get-FTPList -Client $Client
 $List | Format-Table
+# List files within Temporary directory
 $List = Get-FTPList -Client $Client -Path '/Temporary'
 $List | Format-Table
 
+# Get local files
 $ListFiles = Get-ChildItem -LiteralPath $PSScriptRoot\Upload
+
+# Upload file by file
 foreach ($File in $ListFiles) {
-    Add-FTPFile -Client $Client -LocalPath $File.FullName -RemotePath "/Temporary/$($File.Name)" -RemoteExists Skip
+    # To temporary
+    Add-FTPFile -Client $Client -LocalPath $File.FullName -RemotePath "/Temporary/$($File.Name)" -RemoteExists Overwrite
+    # to directory within Temporary that may not exists
     Add-FTPFile -Client $Client -LocalPath $File.FullName -RemotePath "/Temporary/CreateDir/$($File.Name)" -RemoteExists Skip -CreateRemoteDirectory
 }
+
+# Upload all files at once to FTP
+Add-FTPFile -Client $Client -LocalPath $ListFiles.FullName -RemotePath "/Temporary" -RemoteExists Overwrite
 
 Disconnect-FTP -Client $Client
