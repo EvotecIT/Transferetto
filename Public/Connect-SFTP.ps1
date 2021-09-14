ffb0f51138a4c5f0efc54135e59f97dd24400203
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-14 22:08:35 +0200

    Improved error handling #2

--- Public/Connect-SFTP.ps1
+++ Public/Connect-SFTP.ps1
@@ -1,21 +1,21 @@
 ﻿function Connect-SFTP {
     [cmdletBinding(DefaultParameterSetName = 'Password')]
     param(
-        [Parameter(ParameterSetName = 'ClearText')]
-        [Parameter(ParameterSetName = 'Password')]
-        [Parameter(ParameterSetName = 'PrivateKey')]
+        [Parameter(ParameterSetName = 'ClearText', Mandatory)]
+        [Parameter(ParameterSetName = 'Password', Mandatory)]
+        [Parameter(ParameterSetName = 'PrivateKey', Mandatory)]
         [string] $Server,
 
-        [Parameter(ParameterSetName = 'ClearText')]
-        [Parameter(ParameterSetName = 'PrivateKey')]
+        [Parameter(ParameterSetName = 'ClearText', Mandatory)]
+        [Parameter(ParameterSetName = 'PrivateKey', Mandatory)]
         [string] $Username,
 
-        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'ClearText', Mandatory)]
         [string] $Password,
 
-        [Parameter(ParameterSetName = 'Password')]
+        [Parameter(ParameterSetName = 'Password', Mandatory)]
         [pscredential] $Credential,
-        
+
         [Parameter(Mandatory, ParameterSetName = 'PrivateKey')]
         [string] $PrivateKey,
 
@@ -24,17 +24,37 @@
         [Parameter(ParameterSetName = 'PrivateKey')]
         [int] $Port
     )
-
-    if ($Username -and $Password) {
-        $SftpClient = [Renci.SshNet.SftpClient]::new($Server, $Username, $Password)
-    } elseif ($Credential) {
-        $SftpClient = [Renci.SshNet.SftpClient]::new($Server, $Credential.Username, $Credential.GetNetworkCredential().Password)
-    } elseif ($PrivateKey) {
-        [string]$PrivateKey = Resolve-Path $PrivateKey | Select-Object -ExpandProperty ProviderPath
-        $SftpClient = [Renci.SshNet.SftpClient]::new($Server, $Username, [Renci.SshNet.PrivateKeyFile]$PrivateKey )
-    } else {
-        throw 'Not implemented and unexepected.'
+    try {
+        if ($Username -and $Password) {
+            $SftpClient = [Renci.SshNet.SftpClient]::new($Server, $Username, $Password)
+        } elseif ($Credential) {
+            $SftpClient = [Renci.SshNet.SftpClient]::new($Server, $Credential.Username, $Credential.GetNetworkCredential().Password)
+        } elseif ($PrivateKey) {
+            if (Test-Path -LiteralPath $PrivateKey) {
+                [string]$PrivateKey = Resolve-Path -LiteralPath $PrivateKey -ErrorAction Stop | Select-Object -ExpandProperty ProviderPath
+                $SftpClient = [Renci.SshNet.SftpClient]::new($Server, $Username, [Renci.SshNet.PrivateKeyFile]$PrivateKey )
+            } else {
+                if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+                    throw "PrivateKey $PrivateKey doesn't exists."
+                    return
+                } else {
+                    Write-Warning "Connect-SFTP - PrivateKey $PrivateKey doesn't exists."
+                    return
+                }
+            }
+        } else {
+            throw 'Not implemented and unexepected.'
+            return
+        }
+    } catch {
+        if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+            Write-Error $_
+            return
+        } else {
+            Write-Warning "Connect-SFTP - Error: $($_.Exception.Message)"
+        }
     }
+
     if ($Port) {
         $SftpClient.Port = $Port
     }
