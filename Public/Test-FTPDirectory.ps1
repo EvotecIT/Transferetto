899d52e5ef7a5991b8bf0724dfec832f28df6f2a
Tobias Kleinmann <19623985+Sidewinder53@users.noreply.github.com>
2021-09-15 20:05:14 +0100

    Add Test-FTPDirectory and data file reference

---
+++ Public/Test-FTPDirectory.ps1
@@ -0,0 +1,10 @@
+﻿function Test-FTPDirectory {
+    [cmdletBinding()]
+    param(
+        [Parameter(Mandatory)][FluentFTP.FtpClient] $Client,
+        [Parameter(Mandatory)][string] $RemotePath
+    )
+    if ($Client -and $Client.IsConnected -and -not $Client.Error) {
+        $Client.DirectoryExists($RemotePath)
+    }
+}
