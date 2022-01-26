c2e9e0623b0aa36e5ceeac4d0d4a8b361d21c9ad
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-26 20:22:56 +0100

    Improve Send-FTPDirectory with docs and more options #10

--- Public/Send-FTPDirectory.ps1
+++ Public/Send-FTPDirectory.ps1
@@ -1,4 +1,51 @@
 ﻿function Send-FTPDirectory {
+    <#
+    .SYNOPSIS
+    Uploads a directory to an FTP server.
+
+    .DESCRIPTION
+    Uploads a directory to an FTP server.
+
+    .PARAMETER Client
+    The Client to use for connection.
+
+    .PARAMETER LocalPath
+    Path on the local machine to upload to FTP Server
+
+    .PARAMETER RemotePath
+    Path on the FTP Server where to upload the content
+
+    .PARAMETER FolderSyncMode
+    Update - upload a folder and all its files
+    Mirror - upload a folder and all its files, and delete extra files on the server
+
+    .PARAMETER RemoteExists
+    Provide decision what to do when file on the server exits.
+    Options available: Append, AppendNoChek, NoCheck, Skip, , Overwrite
+    Default: Skip.
+
+    .PARAMETER VerifyOptions
+    Provide options for verification of files on the remote server.
+    Options available: Delete, OnlyChecksum, None, Retry
+    Default: None.
+
+    .PARAMETER Rules
+    Provide rules and conditions for uploading files.
+
+    .EXAMPLE
+    Set-FTPTracing -Enable -DisplayConsole
+
+    $Client = Connect-FTP -Server '192.168.241.187' -Verbose -Username 'test' -Password 'BiPassword90A' -EncryptionMode Explicit -ValidateAnyCertificate
+    $Upload = Send-FTPDirectory -Client $Client -LocalPath $PSScriptRoot\Upload -RemotePath '/Temporary' -Verbose -FolderSyncMode Update
+    $Upload | Format-Table *
+
+    Disconnect-FTP -Client $Client
+
+    Set-FTPTracing -Disable
+
+    .NOTES
+    General notes
+    #>
     [alias('Add-FTPDirectory')]
     [cmdletBinding()]
     param(
@@ -6,15 +53,12 @@
         [string] $LocalPath,
         [Parameter(Mandatory)][string] $RemotePath,
         [FluentFTP.FtpFolderSyncMode] $FolderSyncMode = [FluentFTP.FtpFolderSyncMode]::Update,
-        [FluentFTP.FtpRemoteExists] $RemoteExists,
-        [FluentFTP.FtpVerify] $VerifyOptions,
+        [FluentFTP.FtpRemoteExists] $RemoteExists = [FluentFTP.FtpRemoteExists]::Skip,
+        [FluentFTP.FtpVerify] $VerifyOptions = [FluentFTP.FtpVerify]::None,
         [FluentFTP.Rules.FtpRule[]] $Rules
     )
     if ($Client -and $Client.IsConnected -and -not $Client.Error) {
-        # System.Collections.Generic.List[FluentFTP.FtpResult] UploadDirectory(string localFolder, string remoteFolder, FluentFTP.FtpFolderSyncMode mode, FluentFTP.FtpRemoteExists existsMode, FluentFTP.FtpVerify verifyOptions, System.Collections.Generic.List[FluentFTP.Rules.FtpRule] rules, System.Action[FluentFTP.FtpProgress] progress)
-        # System.Collections.Generic.List[FluentFTP.FtpResult] IFtpClient.UploadDirectory(string localFolder, string remoteFolder, FluentFTP.FtpFolderSyncMode mode, FluentFTP.FtpRemoteExists existsMode, FluentFTP.FtpVerify verifyOptions, System.Collections.Generic.List[FluentFTP.Rules.FtpRule] rules, System.Action[FluentFTP.FtpProgress] progress)
-
-        $Client.UploadDirectory($LocalPath, $RemotePath, $FolderSyncMode) #, $RemoteExists, $VerifyOptions, @($Rules))
-
+        #$Client.UploadDirectory($LocalPath, $RemotePath, $FolderSyncMode)
+        $Client.UploadDirectory($LocalPath, $RemotePath, $FolderSyncMode, $RemoteExists, $VerifyOptions, @($Rules))
     }
 }
