10776a98d69afcdd2ff06b020de0e427c52024a0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-04-10 14:24:35 +0200

    Updated docs

--- Public/Set-FTPTracing.ps1
+++ Public/Set-FTPTracing.ps1
@@ -1,11 +1,48 @@
 ﻿function Set-FTPTracing {
+    <#
+    .SYNOPSIS
+    Allows enabling/disabling tracing ftp commands being sent and received during communucation with the server.
+
+    .DESCRIPTION
+    Allows enabling/disabling tracing ftp commands being sent and received during communucation with the server.
+
+    .PARAMETER LogPath
+    Set this to a file path to append all FTP communication to it. Default: false.
+
+    .PARAMETER Enable
+    Enable tracing
+
+    .PARAMETER Disable
+    Disable tracing
+
+    .PARAMETER ShowPassword
+    Include FTP passwords in logs? Default: false.
+
+    .PARAMETER HideUserName
+    Hide FTP usernames in logs? Default: false.
+
+    .PARAMETER HideIP
+    Hide server IP addresses in logs? Default: true.
+
+    .PARAMETER HideFunctions
+    Hide high-level function calls in logs? Default: false.
+
+    .PARAMETER DisplayConsole
+    Should FTP communication be be logged to the console? Default: false.
+
+    .EXAMPLE
+    Set-FTPTracing -Enable -DisplayConsole
+
+    .NOTES
+    General notes
+    #>
     [cmdletBinding()]
     param(
         [string] $LogPath,
         [switch] $Enable,
         [switch] $Disable,
         [switch] $ShowPassword,
-        [switch] $ShowUsername,
+        [switch] $HideUserName,
         [switch] $HideIP,
         [switch] $HideFunctions,
         [switch] $DisplayConsole
@@ -26,8 +63,8 @@
     if ($DisplayConsole) {
         [FluentFTP.FtpTrace]::LogToConsole = $true
     }
-    if ($ShowUsername) {
-        [FluentFTP.FtpTrace]::LogUserName = $true; # hide FTP user names
+    if ($HideUserName) {
+        [FluentFTP.FtpTrace]::LogUserName = $false; # hide FTP user names
     }
     if ($ShowPassword) {
         [FluentFTP.FtpTrace]::LogPassword = $false; # hide FTP passwords
