ff9e0ba0cb85c57fff71fb416e66ed31ef5e7b9b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-05 16:47:35 +0100

    Examples for proxy #19

---
+++ Examples/Example15-BasicExampleFTPwithProxy.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module .\Transferetto.psd1 -Force
+
+# Login via UserName/Password via proxy
+$Client = Connect-FTP -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password' -ProxyHost '192.252.216.81' -ProxyPort 4145 -ProxyType FtpClientSocks5Proxy
+$List = Get-FTPList -Client $Client
+$List | Format-Table
+Disconnect-FTP -Client $Client
+
+
+# Login via UserName/Password with Proxy
+$Client = Connect-FTP -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password' -ProxyHost '104.37.135.145' -ProxyPort 4145 -ProxyType FtpClientSocks4aProxy
+$List = Get-FTPList -Client $Client
+$List | Format-Table
+Disconnect-FTP -Client $Client
+
+
+# # Login via UserName/Password via proxy
+# $Client = Connect-FTP -Server 'ftp.gnu.org' -Verbose -ProxyHost '192.252.216.81' -ProxyPort 4145 -ProxyType FtpClientSocks5Proxy
+# $List = Get-FTPList -Client $Client
+# $List | Format-Table
+# Disconnect-FTP -Client $Client
