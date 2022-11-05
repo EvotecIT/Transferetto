b3c2890db6034f0df6c76e1fefa0e69142c7d355
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-05 16:43:32 +0100

    Adds experimental proxy support #19

--- Public/Connect-FTP.ps1
+++ Public/Connect-FTP.ps1
@@ -2,6 +2,43 @@
     [cmdletBinding(DefaultParameterSetName = 'Password')]
     param(
         [Parameter(ParameterSetName = 'FtpProfile')]
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [string] $ProxyHost,
+
+        [Parameter(ParameterSetName = 'FtpProfile')]
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [int] $ProxyPort,
+
+        [Parameter(ParameterSetName = 'FtpProfile')]
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [pscredential] $ProxyCredential,
+
+        [Parameter(ParameterSetName = 'FtpProfile')]
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [string] $ProxyUserName,
+
+        [Parameter(ParameterSetName = 'FtpProfile')]
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [string] $ProxyPassword,
+
+        [Parameter(ParameterSetName = 'FtpProfile')]
+        [Parameter(ParameterSetName = 'ClearText')]
+        [Parameter(ParameterSetName = 'Password')]
+        [ValidateSet(
+            'FtpClientSocks5Proxy',
+            'FtpClientHttp11Proxy',
+            'FtpClientSocks4aProxy',
+            'FtpClientSocks4Proxy',
+            'FtpClientUserAtHostProxy',
+            'FtpClientBlueCoatProxy'
+        )][string] $ProxyType,
+
+        [Parameter(ParameterSetName = 'FtpProfile')]
         [FluentFTP.FtpProfile] $FtpProfile,
 
         [Parameter(ParameterSetName = 'ClearText')]
@@ -58,61 +95,115 @@
         [Parameter(ParameterSetName = 'Password')]
         [switch] $AutoConnect
     )
+    $ClientInternal = $null
+    if ($PSBoundParameters.ContainsKey('ProxyHost') -or $PSBoundParameters.ContainsKey('ProxyPort') -or $PSBoundParameters.ContainsKey('ProxyType')) {
+        if ($PSBoundParameters.ContainsKey('ProxyHost') -and $PSBoundParameters.ContainsKey('ProxyType')) {
+            $ProxyProfile = [FluentFTP.FtpProxyProfile]::new()
+            $ProxyProfile.ProxyHost = $ProxyHost
+            $ProxyProfile.ProxyPort = $ProxyPort
+
+            if ($ProxyUsername -and $ProxyPassword) {
+                $ProxyProfile.ProxyCredentials = [System.Net.NetworkCredential]::new($Username, $Password)
+            } elseif ($ProxyCredential) {
+                $ProxyProfile.ProxyCredentials = [System.Net.NetworkCredential]::new($ProxyCredential.Username, $ProxyCredential.Password)
+            } else {
+                # anonymous
+            }
+
+            $ProxyProfile.FtpHost = $Server
+            if ($Port) {
+                $ProxyProfile.FtpPort = $Port
+            }
+
+            if ($Username -and $Password) {
+                $ProxyProfile.FTPCredentials = [System.Net.NetworkCredential]::new($Username, $Password)
+            } elseif ($Credential) {
+                $ProxyProfile.FTPCredentials = [System.Net.NetworkCredential]::new($Credential.Username, $Credential.Password)
+            } else {
+                # anonymous
+            }
+
+            if ($ProxyType -eq 'FtpClientSocks5Proxy') {
+                $ClientInternal = [FluentFTP.Proxy.SyncProxy.FtpClientSocks5Proxy]::new($ProxyProfile)
+            } elseif ($ProxyType -eq 'FtpClientHttp11Proxy') {
+                $ClientInternal = [FluentFTP.Proxy.SyncProxy.FtpClientHttp11Proxy]::new($ProxyProfile)
+            } elseif ($ProxyType -eq 'FtpClientSocks4aProxy') {
+                $ClientInternal = [FluentFTP.Proxy.SyncProxy.FtpClientSocks4aProxy]::new($ProxyProfile)
+            } elseif ($ProxyType -eq 'FtpClientSocks4Proxy') {
+                $ClientInternal = [FluentFTP.Proxy.SyncProxy.FtpClientSocks4Proxy]::new($ProxyProfile)
+            } elseif ($ProxyType -eq 'FtpClientUserAtHostProxy') {
+                $ClientInternal = [FluentFTP.Proxy.SyncProxy.FtpClientUserAtHostProxy]::new($ProxyProfile)
+            } elseif ($ProxyType -eq 'FtpClientBlueCoatProxy') {
+                $ClientInternal = [FluentFTP.Proxy.SyncProxy.FtpClientBlueCoatProxy]::new($ProxyProfile)
+            }
+        } else {
+            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+                Write-Error "ProxyHost, ProxyPort, and ProxyType must be specified together when using Proxy. Only ProxyUserName, ProxyPassword or ProxyCredential are optional."
+                return
+            } else {
+                Write-Warning "Connect-FTP - ProxyHost, ProxyPort, and ProxyType must be specified together when using Proxy. Only ProxyUserName, ProxyPassword or ProxyCredential are optional."
+            }
+        }
+    }
     if ($FtpProfile) {
-        $Client = [FluentFTP.FtpClient]::new()
-        $Client.LoadProfile($FtpProfile)
+        if (-not $ClientInternal) {
+            $ClientInternal = [FluentFTP.FtpClient]::new()
+        }
+        $ClientInternal.LoadProfile($FtpProfile)
     } else {
-        $Client = [FluentFTP.FtpClient]::new($Server)
+        if (-not $ClientInternal) {
+            $ClientInternal = [FluentFTP.FtpClient]::new($Server)
+        }
         if ($Username -and $Password) {
-            $Client.Credentials = [System.Net.NetworkCredential]::new($Username, $Password)
+            $ClientInternal.Credentials = [System.Net.NetworkCredential]::new($Username, $Password)
         } elseif ($Credential) {
-            $Client.Credentials = [System.Net.NetworkCredential]::new($Credential.Username, $Credential.Password)
+            $ClientInternal.Credentials = [System.Net.NetworkCredential]::new($Credential.Username, $Credential.Password)
         } else {
             # anonymous
         }
     }
     if ($Port) {
-        $Client.Port = $Port
+        $ClientInternal.Port = $Port
     }
     if ($DataConnectionType) {
-        $Client.Config.DataConnectionType = $DataConnectionType
+        $ClientInternal.Config.DataConnectionType = $DataConnectionType
     }
     if ($DisableDataConnectionEncryption) {
-        $Client.Config.DataConnectionEncryption = $false
+        $ClientInternal.Config.DataConnectionEncryption = $false
     }
     if ($EncryptionMode) {
-        $Client.Config.EncryptionMode  = $EncryptionMode
+        $ClientInternal.Config.EncryptionMode = $EncryptionMode
     }
     if ($ValidateAnyCertificate) {
-        $Client.Config.ValidateAnyCertificate = $true
+        $ClientInternal.Config.ValidateAnyCertificate = $true
     }
     if ($DisableValidateCertificateRevocation) {
-        $Client.Config.ValidateCertificateRevocation = $false
+        $ClientInternal.Config.ValidateCertificateRevocation = $false
     }
     if ($SendHost) {
-        $Client.Config.SendHost = $true
+        $ClientInternal.Config.SendHost = $true
     }
     if ($SocketKeepAlive) {
-        $Client.Config.SocketKeepAlive = $true
+        $ClientInternal.Config.SocketKeepAlive = $true
     }
     if ($FtpsBuffering) {
-        $Client.Config.SslBuffering = $SslBuffering
+        $ClientInternal.Config.SslBuffering = $SslBuffering
     }
     try {
         if ($AutoConnect) {
-            $TempFtpProfile = $Client.AutoConnect()
-            if ($TempFtpProfile -and $Client.IsConnected) {
+            $TempFtpProfile = $ClientInternal.AutoConnect()
+            if ($TempFtpProfile -and $ClientInternal.IsConnected) {
                 Write-Verbose "Following options where used to autoconnect: "
                 foreach ($Name in $TempFtpProfile.PSObject.Properties.Name) {
                     Write-Verbose "[x] $Name -> $($TempFtpProfile.$Name)"
                 }
             }
         } else {
-            $Client.Connect()
+            $ClientInternal.Connect()
         }
-        $Client | Add-Member -Name 'Error' -Value $null -Force -MemberType NoteProperty
+        $ClientInternal | Add-Member -Name 'Error' -Value $null -Force -MemberType NoteProperty
     } catch {
-        $Client | Add-Member -Name 'Error' -Value $($_.Exception.Message) -Force -MemberType NoteProperty
+        $ClientInternal | Add-Member -Name 'Error' -Value $($_.Exception.Message) -Force -MemberType NoteProperty
         if ($PSBoundParameters.ErrorAction -eq 'Stop') {
             Write-Error $_
             return
@@ -120,5 +211,5 @@
             Write-Warning "Connect-FTP - Error: $($_.Exception.Message)"
         }
     }
-    $Client
+    $ClientInternal
 }
--- Tests/Connect-FTP.Tests.ps1
+++ Tests/Connect-FTP.Tests.ps1
@@ -1,7 +1,9 @@
 ﻿Describe 'Connect-FTP / Disconnect-FTP' {
     It 'Given no login and password it should connect to FTP and list' {
         # Anonymous login
+        $Client = $Null
         $Client = Connect-FTP -Server 'ftp.gnu.org' -Verbose
+        $Client.Host | Should -Be 'ftp.gnu.org'
         $Client.IsConnected | Should -Be $True
         $List = Get-FTPList -Client $Client
         $List.Count | Should -BeGreaterThan 5
@@ -14,7 +16,9 @@
     }
     It 'Given login and password and Encryption Mode it should connect to FTP and list' {
         # Anonymous login
+        $Client = $Null
         $Client = Connect-FTP -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password' -EncryptionMode Auto -SslBuffering Auto -SocketKeepAlive
+        $Client.Host | Should -Be 'test.rebex.net'
         $Client.IsConnected | Should -Be $True
         $List = Get-FTPList -Client $Client
         $List.Count | Should -Be 2
@@ -27,7 +31,9 @@
     }
     It 'Given login and password it should connect to FTP' {
         # Login via UserName/Password
+        $Client = $Null
         $Client = Connect-FTP -Server 'ftp.dlptest.com' -Verbose -Username 'dlpuser' -Password 'rNrKYTX9g7z3RgJRmxWuGHbeu'
+        $Client.Host | Should -Be 'ftp.dlptest.com'
         $Client.IsConnected | Should -Be $True
         $List = Get-FTPList -Client $Client
         $List.Count | Should -BeGreaterThan 1
@@ -38,4 +44,36 @@
         Disconnect-FTP -Client $Client
         $Client.IsConnected | Should -Be $false
     }
+    It 'Given login and password, and proxy Socks 5 it should conntect and list' {
+        # Anonymous login
+        $Client = $Null
+        $Client = Connect-FTP -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password' -EncryptionMode Auto -SslBuffering Auto -SocketKeepAlive -ProxyHost '192.252.216.81' -ProxyPort 4145 -ProxyType FtpClientSocks5Proxy
+        $Client.Host | Should -Be 'test.rebex.net'
+        $Client.IsConnected | Should -Be $True
+        $List = Get-FTPList -Client $Client
+        $List.Count | Should -Be 2
+        $Names = 'Name', 'Type', 'FullName', 'Modified', 'Created'
+        foreach ($Name in  @($Names)) {
+            $List[0].PSObject.Properties.Name | Should -Contain $Name
+        }
+        Disconnect-FTP -Client $Client
+        $Client.IsConnected | Should -Be $false
+    }
+
+    # It 'Given login and password, and proxy Socks 4a it should conntect and list' {
+    #     # Anonymous login
+    #     $Client = $Null
+    #     $Client = Connect-FTP -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password' -EncryptionMode Auto -SslBuffering Auto -SocketKeepAlive -ProxyHost '104.37.135.145' -ProxyPort 4145 -ProxyType FtpClientSocks4aProxy
+    #     $Client.Host | Should -Be 'test.rebex.net'
+    #     $Client.IsConnected | Should -Be $True
+    #     $List = Get-FTPList -Client $Client
+    #     $List.Count | Should -Be 2
+    #     $Names = 'Name', 'Type', 'FullName', 'Modified', 'Created'
+    #     foreach ($Name in  @($Names)) {
+    #         $List[0].PSObject.Properties.Name | Should -Contain $Name
+    #     }
+    #     Disconnect-FTP -Client $Client
+    #     $Client.IsConnected | Should -Be $false
+    # }
+
 }
