39a6db7992e1eeba64beac4f5cf61db022b581a5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-11-05 14:20:50 +0100

    Improve tests to cover #20

--- Tests/Connect-FTP.Tests.ps1
+++ Tests/Connect-FTP.Tests.ps1
@@ -12,6 +12,19 @@
         Disconnect-FTP -Client $Client
         $Client.IsConnected | Should -Be $false
     }
+    It 'Given login and password and Encryption Mode it should connect to FTP and list' {
+        # Anonymous login
+        $Client = Connect-FTP -Server 'test.rebex.net' -Verbose -Username 'demo' -Password 'password' -EncryptionMode Auto
+        $Client.IsConnected | Should -Be $True
+        $List = Get-FTPList -Client $Client
+        $List.Count | Should -Be 2
+        $Names = 'Name', 'Type', 'FullName', 'Modified', 'Created'
+        foreach ($Name in  @($Names)) {
+            $List[0].PSObject.Properties.Name | Should -Contain $Name
+        }
+        Disconnect-FTP -Client $Client
+        $Client.IsConnected | Should -Be $false
+    }
     It 'Given login and password it should connect to FTP' {
         # Login via UserName/Password
         $Client = Connect-FTP -Server 'ftp.dlptest.com' -Verbose -Username 'dlpuser' -Password 'rNrKYTX9g7z3RgJRmxWuGHbeu'
