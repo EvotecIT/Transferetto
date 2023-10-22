0b64e16d66a4690c5fda74de6bc6a927af2aab53
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-22 13:30:44 +0200

    Fix output

--- Examples/Example12-Receive-SFTPFile.ps1
+++ Examples/Example12-Receive-SFTPFile.ps1
@@ -1,20 +1,20 @@
+Clear-Host
 Import-Module .\Transferetto.psd1 -Force
 
 # Connect to SFTP Server
 $SftpClient = Connect-SFTP -Server 'test.rebex.net' -Username demo -Password password
 
 # Get All Files in '/test' for Export
-$Export_Files = Get-SFTPList -SftpClient $SftpClient -Path '/pub/example' | Where-Object {$_.IsDirectory -eq $false} 
+$Export_Files = Get-SFTPList -SftpClient $SftpClient -Path '/pub/example' | Where-Object { $_.IsDirectory -eq $false }
 
 # Set Export Directory
-$ExportPathLocal = "C:\Temp\sftp_demo"
+$ExportPathLocal = "C:\Temp"
 
 # Download Each File
-ForEach ($RemoteFile in $Export_Files) {
-
+$Output = ForEach ($RemoteFile in $Export_Files) {
     Receive-SFTPFile -SftpClient $SftpClient -RemotePath $RemoteFile.FullName -LocalPath "$ExportPathLocal\$($RemoteFile.Name)"
-
 }
+$Output | Format-Table
 
 # Disconnect
 Disconnect-FTP -Client $Client
