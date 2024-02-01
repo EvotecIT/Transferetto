3c5ee02cb17b97ff4ca1ae6383b9bac194e07b95
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-01 12:40:21 +0100

    Update changelog

--- Build/Manage-Module.ps1
+++ Build/Manage-Module.ps1
@@ -143,6 +143,6 @@ Build-Module -ModuleName 'Transferetto' {
     #New-ConfigurationTest -TestsPath "$PSScriptRoot\..\Tests" -Enable
 
     # global options for publishing to github/psgallery
-    New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
-    New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
+    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
+    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
 } -ExitCode
