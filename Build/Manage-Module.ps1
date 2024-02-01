36d442ab1311afe32325ac3e4ad7244503f097ab
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-01 12:38:57 +0100

    Update module builder

--- Build/Manage-Module.ps1
+++ Build/Manage-Module.ps1
@@ -6,32 +6,32 @@ Build-Module -ModuleName 'Transferetto' {
     # Usual defaults as per standard module
     $Manifest = [ordered] @{
         # Version number of this module.
-        ModuleVersion          = '0.0.X'
+        ModuleVersion        = '0.0.X'
         # Supported PSEditions
-        CompatiblePSEditions   = @('Desktop', 'Core')
+        CompatiblePSEditions = @('Desktop', 'Core')
         # ID used to uniquely identify this module
-        GUID                   = '7d61db15-9efe-41d1-a1c0-81d738975dec'
+        GUID                 = '7d61db15-9efe-41d1-a1c0-81d738975dec'
         # Author of this module
-        Author                 = 'Przemyslaw Klys'
+        Author               = 'Przemyslaw Klys'
         # Company or vendor of this module
-        CompanyName            = 'Evotec'
+        CompanyName          = 'Evotec'
         # Copyright statement for this module
-        Copyright              = "(c) 2011 - $((Get-Date).Year) Przemyslaw Klys @ Evotec. All rights reserved."
+        Copyright            = "(c) 2011 - $((Get-Date).Year) Przemyslaw Klys @ Evotec. All rights reserved."
         # Description of the functionality provided by this module
-        Description            = 'Module which allows ftp, ftps, sftp file transfers with advanced features. It also allows to transfer files and directorires between servers using fxp protocol. As a side feature it allows to conenct to SSH and executes commands on it. '
+        Description          = 'Module which allows ftp, ftps, sftp file transfers with advanced features. It also allows to transfer files and directorires between servers using fxp protocol. As a side feature it allows to conenct to SSH and executes commands on it. '
         # Minimum version of the Windows PowerShell engine required by this module
-        PowerShellVersion      = '5.1'
+        PowerShellVersion    = '5.1'
         # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
         # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
-        Tags                   = @('Windows', 'Linux', 'MacOs', 'ftp', 'sftp', 'ftps', 'scp', 'winscp', 'ssh')
+        Tags                 = @('Windows', 'Linux', 'MacOs', 'ftp', 'sftp', 'ftps', 'scp', 'winscp', 'ssh')
 
-        IconUri                = 'https://evotec.xyz/wp-content/uploads/2021/03/Transferetto.png'
+        IconUri              = 'https://evotec.xyz/wp-content/uploads/2021/03/Transferetto.png'
 
-        ProjectUri             = 'https://github.com/EvotecIT/Transferetto'
+        ProjectUri           = 'https://github.com/EvotecIT/Transferetto'
 
-       # DotNetFrameworkVersion = '4.7.2'
+        # DotNetFrameworkVersion = '4.7.2'
     }
-    New-ConfigurationManifest @Manifest
+    New-ConfigurationManifest @Manifest #-Prerelease 'Alpha1'
     # Add external module dependencies, using loop for simplicity
     New-ConfigurationModule -Type ExternalModule -Name 'Microsoft.PowerShell.Management', 'Microsoft.PowerShell.Utility'
 
@@ -102,9 +102,17 @@ Build-Module -ModuleName 'Transferetto' {
         DotSourceClasses                  = $true
         #SeparateFileLibraries             = $true
         DeleteTargetModuleBeforeBuild     = $true
+        NETIgnoreLibraryOnLoad            = @(
+            'libgcc_s_seh-1.dll'
+            'libgmp-10.dll'
+            'libgnutls-30.dll'
+            'libhogweed-6.dll'
+            'libnettle-8.dll'
+            'libwinpthread-1.dll'
+        )
     }
 
-    New-ConfigurationBuild @newConfigurationBuildSplat  #-DotSourceLibraries -DotSourceClasses -MergeModuleOnBuild -Enable -SignModule -DeleteTargetModuleBeforeBuild -CertificateThumbprint '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703' -MergeFunctionsFromApprovedModules
+    New-ConfigurationBuild @newConfigurationBuildSplat
 
     $newConfigurationArtefactSplat = @{
         Type                = 'Unpacked'
@@ -135,6 +143,6 @@ Build-Module -ModuleName 'Transferetto' {
     #New-ConfigurationTest -TestsPath "$PSScriptRoot\..\Tests" -Enable
 
     # global options for publishing to github/psgallery
-    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
-    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
+    New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
+    New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
 } -ExitCode
